1d39964636cbbff72c2722eba8df88a0b933eec5
Артур Дученко <arthur@fulcrum.rocks>
2020-07-11 02:00:15 +0300

    Add fix to install package

--- src/utils/installPackage.js
+++ src/utils/installPackage.js
@@ -1,3 +1,5 @@
+import chalk from "chalk";
+
 const packageList = [
   "redux",
   "react-redux",
@@ -16,11 +18,24 @@ const packageList = [
   "@react-navigation/stack",
 ];
 
-export default function installAllPackage() {
-  packageList.forEach((item) => {
-    var exec = require("child_process").exec,
-      child;
+function execShellCommand(cmd) {
+  const exec = require("child_process").exec;
 
-    child = exec(`npm install --save ${item}`).stderr.pipe(process.stderr);
+  console.log(chalk.yellow(`Command ${cmd} is running`));
+  exec(`npm install --save ${cmd}`, (error, stdout, stderr) => {
+    if (error) {
+      console.log(chalk.red("Error:", error));
+    }
+
+    stdout
+      ? console.log(chalk.green(`Success install package`, stdout))
+      : console.log(chalk.green(`Error  install package`, stderr));
+  });
+}
+
+export default function installAllPackage() {
+  console.log(chalk.yellow("\nPlease wait, we install the packages\n"));
+  packageList.forEach((item) => {
+    execShellCommand(item);
   });
 }
