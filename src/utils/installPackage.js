90891d9ce609c2c38a55b5da5859f668124f8f54
Arthur Duchenko <arthur@fulcrum.rocks>
2020-08-07 16:12:51 +0300

    Fix problem with install dependencies

--- src/utils/installPackage.js
+++ src/utils/installPackage.js
@@ -40,7 +40,7 @@ function execShellCommand(cmd) {
     }
 
     stdout
-      ? console.log(chalk.green(`Success install package`, stdout))
+      ? console.log(chalk.green(`Success install package\n`, stdout))
       : console.log(chalk.green(`Error  install package`, stderr));
   });
 }
@@ -57,7 +57,7 @@ function execShellCommandDev(cmd) {
     }
 
     stdout
-      ? console.log(chalk.green(`Success install package`, stdout))
+      ? console.log(chalk.green(`Success install package\n`, stdout))
       : console.log(chalk.green(`Error  install package`, stderr));
   });
 }
@@ -65,19 +65,13 @@ function execShellCommandDev(cmd) {
 export function installAllTsPackage() {
   console.log(chalk.yellow("\nPlease wait, we install dependencies\n"));
 
-  packageList.forEach((item) => {
-    execShellCommand(item);
-  });
+  execShellCommand(packageList.join(" "));
 
-  tsPackageList.forEach((item) => {
-    execShellCommandDev(item);
-  });
+  execShellCommandDev(tsPackageList.join(" "));
 }
 
 export default function installAllJsPackage() {
   console.log(chalk.yellow("\nPlease wait, we install dependencies\n"));
 
-  packageList.forEach((item) => {
-    execShellCommand(item);
-  });
+  execShellCommand(packageList.join(" "));
 }
