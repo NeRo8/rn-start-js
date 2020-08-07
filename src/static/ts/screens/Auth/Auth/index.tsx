12cb3e9d4c6febeee567d49d0d4569e7909c6d2f
Arthur Duchenko <arthur@fulcrum.rocks>
2020-08-07 15:02:02 +0300

    Add multilanguage to Auth

--- src/static/ts/screens/Auth/Auth/index.tsx
+++ src/static/ts/screens/Auth/Auth/index.tsx
@@ -1,22 +1,23 @@
-import React, {useState, useEffect} from 'react';
-import {SafeAreaView, Text, Switch} from 'react-native';
-import {useDispatch} from 'react-redux';
+import React, { useState, useEffect } from "react";
+import { SafeAreaView, Text, Switch } from "react-native";
+import { useDispatch } from "react-redux";
 
-import styles from './styles';
-import {translate} from '../../../i18n';
-import {themeActions} from '../../../redux/themes';
+import styles from "./styles";
+import setI18nConfig, { translate } from "../../../i18n";
+import { themeActions } from "../../../redux/themes";
 
 function Auth() {
   const [isActive, changeTheme] = useState(false);
   const dispatch = useDispatch();
   useEffect(() => {
-    const themeName = isActive ? 'dark' : 'light';
+    const themeName = isActive ? "dark" : "light";
     dispatch(themeActions.changeThemeMode(themeName));
+    setI18nConfig({ languageTag: isActive ? "ru" : "en", isRTL: false });
   }, [isActive]);
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.text}>{translate('hello')}</Text>
+      <Text style={styles.text}>{translate("hello")}</Text>
       <Switch value={isActive} onValueChange={() => changeTheme(!isActive)} />
     </SafeAreaView>
   );
