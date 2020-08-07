9ba37140ffa64920f893d54e473111be89adb692
Arthur Duchenko <arthur@fulcrum.rocks>
2020-08-07 14:22:05 +0300

    Add config ts files

--- src/cli.js
+++ src/cli.js
@@ -6,29 +6,40 @@ var path = require("path");
 var fs = require("fs-extra");
 
 var sourceJS = path.join(__dirname, "/static/js");
+
 var sourceTS = path.join(__dirname, "/static/ts");
+var sourceTsConfigFiles = path.join(__dirname, "static/configFiles");
+
 var destination = process.cwd() + "/src";
+var destinationTsConfig = process.cwd();
 
 function installJs() {
-  fs.copy(source, destination, function (err) {
+  fs.copy(sourceJS, destination, function (err) {
     if (err) {
       console.log("An error occured while copying the folder.");
       return console.error(err);
     }
   });
 
-  // installAllJsPackage();
+  installAllJsPackage();
 }
 
 function installTs() {
-  fs.copy(source, destination, function (err) {
+  fs.copy(sourceTS, destination, function (err) {
     if (err) {
       console.log("An error occured while copying the folder.");
       return console.error(err);
     }
   });
 
-  // installAllTsPackage();
+  fs.copy(sourceTsConfigFiles, destinationTsConfig, function (err) {
+    if (err) {
+      console.log("An error occured while copying the folder.");
+      return console.error(err);
+    }
+  });
+
+  installAllTsPackage();
 }
 
 export function cli(args) {
